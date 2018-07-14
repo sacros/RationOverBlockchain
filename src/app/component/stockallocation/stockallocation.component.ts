a30e19273e773c03766e5bad0df060ff68d2c339
Sahil Jhamb <sahiljhamb11@gmail.com>
2018-07-14 14:41:55 +0530

    stockAllocation Changes

--- src/app/component/stockallocation/stockallocation.component.ts
+++ src/app/component/stockallocation/stockallocation.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Stock } from '../../../model/Stock';
+import { Web3Service } from '../../util/web3.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+declare let require: any;
+const rdsArtifacts = require('../../../../build/contracts/RDS.json');
 
 @Component({
   selector: 'app-stockallocation',
@@ -6,10 +12,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stockallocation.component.css']
 })
 export class StockallocationComponent implements OnInit {
+  commodity: Stock = new Stock();
+  accounts: string[];
+  RdsContract: any;
+  model = {
+    amount: 5,
+    receiver: '',
+    balance: 0,
+    account: ''
+  };
 
-  constructor() { }
-
+  constructor(private web3Service: Web3Service, private router: Router) { }
   ngOnInit() {
+    this.watchAccount();
+    this.web3Service.artifactsToContract(rdsArtifacts)
+      .then((RdsAbstraction) => {
+        this.RdsContract = RdsAbstraction;
+        // this.callTestMethod();
+      });
   }
 
+  watchAccount() {
+    this.web3Service.accountsObservable.subscribe((accounts) => {
+      this.accounts = accounts;
+      this.model.account = accounts[0];
+      console.log('Account', this.model.account);
+      //  this.refreshBalance();
+      ////////////
+    });
+  }
 }
---
+++ src/model/Stock.ts
@@ -0,0 +1,6 @@
+export class Stock {
+    Rice:string = "0";
+    Wheat:string = "0";
+    Kerosene:string = "0";
+    UserCategory:string = "0";
+}
