dbefcb429f85a3b619c01c61b8e20ea550ec975b
Chandan Kumar <chandan.kumar@svam.com>
2018-07-14 13:54:01 +0530

    solve app module conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './component/layout/layout.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
-<<<<<<< Updated upstream
 import { CommodityRatesComponent } from './component/commodity-rates/commodity-rates.component';
 import { StockallocationComponent } from './component/stockallocation/stockallocation.component';
 import { RationallocationComponent } from './component/rationallocation/rationallocation.component';
+import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login',pathMatch: 'full'},
+  {
+    path: 'login', component: LoginComponent
+  },
   {
     path: 'commodityRates', component: LayoutComponent,
     children: [
@@ -46,26 +49,6 @@ const routes: Routes = [
       }
     ]
   },
-
-=======
-import { LoginComponent } from './component/login/login.component';
-
-const routes: Routes = [
-  {
-    path: 'login', component: LoginComponent
-  },
-
-{ path: 'dashboard', component:DashboardComponent, 
-      children: [
-                  {
-                      path: '',
-                      component: DashboardComponent                         
-                  }
-                ]
-   },
-   { path: '', redirectTo: '/login',pathMatch: 'full'},
->>>>>>> Stashed changes
-
   { path: '**', redirectTo: '' }
 ];
 
