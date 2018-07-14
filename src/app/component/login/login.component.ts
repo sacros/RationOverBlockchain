647bc804a94bfa97c5f77e551404b0adb56d7fd8
Chandan Kumar <chandan.kumar@svam.com>
2018-07-14 15:23:05 +0530

    login

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Register } from '../../../model/Register';
+import { Web3Service } from '../../util/web3.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ConversionService } from '../../service/conversion.service';
+import { IpfsService } from '../../service/ipfs.service';
+
+declare let require: any;
+const rdsArtifacts = require('../../../../build/contracts/RDS.json');
 
 @Component({
   selector: 'app-login',
@@ -7,13 +15,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
   dependent = {'addharNo':''};
   dependentAadhars = [];
   noOfDependents:number;
   count : number =0;
   url: string;
+  register: Register = new Register();
+  accounts: string[];
+  RdsContract: any;
+  model = {
+    amount: 5,
+    receiver: '',
+    balance: 0,
+    account: ''
+  };
+
+  constructor(private web3Service: Web3Service, private IpfsService: IpfsService, private converionService: ConversionService, private router: Router) { }
+
   ngOnInit() {
+    this.watchAccount();
+    this.web3Service.artifactsToContract(rdsArtifacts)
+      .then((RdsAbstraction) => {
+        this.RdsContract = RdsAbstraction;
+        // this.callTestMethod();
+      });
+  }
+
+  watchAccount() {
+    this.web3Service.accountsObservable.subscribe((accounts) => {
+      this.accounts = accounts;
+      this.model.account = accounts[0];
+      console.log('Account', this.model.account);
+      //  this.refreshBalance();
+      ////////////
+    });
   }
 
   addDependents(){
@@ -25,8 +60,31 @@ export class LoginComponent implements OnInit {
 
   registerAdmin(){
     alert("called");
-    // this.registerData
-    this.url = "";
+    var data = JSON.stringify(this.register);
+    this.IpfsService.add(data).then((hash) => {
+        alert('hash: ' + hash);
+        this.registerData(this.register, hash);
+    })
+  }
+
+  async registerData(register, hash) {
+    console.log('calling method from contract');
+    try {
+      const deployedContract = await this.RdsContract.deployed();
+      console.log(deployedContract);
+      //console.log('Account', this.model.account);
+      
+      //let callRegister = await deployedContract.registerIndividual(this.register.UserCategory, this.commodity.Wheat, this.commodity.Rice, this.commodity.Kerosene, {from: this.model.account});
+      
+      console.log('Contract called');
+
+      // @TODO: Redirect to appropriate page
+      this.router.navigate(["/dashboard"]);
+
+      //const metaCoinBalance = await deployedLand.getBalance.call(this.model.account);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
 }
